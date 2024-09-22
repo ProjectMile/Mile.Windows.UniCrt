1e61be109a3269157f511ea574a541c1b638ceae
MouriNaruto <Kenji.Mouri@outlook.com>
2024-09-22 18:41:26 +0800

    Add original style Mile.UniCrt.Wrapper.

---
+++ Mile.UniCrt.Wrapper/Mile.UniCrt.Wrapper.cpp
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.UniCrt.Wrapper.cpp
+ * PURPOSE:   Implementation for Mile.UniCrt.Wrapper
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include <Windows.h>
+
+EXTERN_C int WINAPI MileUniCrtWrapperDummyFunction()
+{
+    return 0;
+}

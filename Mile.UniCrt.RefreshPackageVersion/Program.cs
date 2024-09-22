8508ff86dfc321bde26e203adea8c166728777a5
MouriNaruto <Kenji.Mouri@outlook.com>
2024-09-22 19:36:03 +0800

    Add Mile.UniCrt.RefreshPackageVersion project.

---
+++ Mile.UniCrt.RefreshPackageVersion/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Mile.UniCrt.RefreshPackageVersion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "{0} task has been completed.",
+                "Mile.UniCrt.RefreshPackageVersion");
+            Console.ReadKey();
+        }
+    }
+}

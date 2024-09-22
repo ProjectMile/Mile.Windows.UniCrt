55908b7596ad8e2f69a5eb4a4f6e2f6ccc317fa9
MouriNaruto <Kenji.Mouri@outlook.com>
2024-09-22 13:27:05 +0800

    Add Mile.UniCrt.WrapperDefinitionGenerator project.

---
+++ Mile.UniCrt.WrapperDefinitionGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Mile.UniCrt.WrapperDefinitionGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "{0} task has been completed.",
+                "Mile.UniCrt.WrapperDefinitionGenerator");
+            Console.ReadKey();
+        }
+    }
+}

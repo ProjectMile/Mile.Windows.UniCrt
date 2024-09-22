a097ad608f992e8cd3bdf2b1da62408b26cb8507
MouriNaruto <Kenji.Mouri@outlook.com>
2024-09-22 12:01:10 +0800

    Implement Mile.UniCrt.Build.Tasks.SimplifiedBaseDefinitionGenerator.

---
+++ Mile.UniCrt.Build.Tasks/SimplifiedBaseDefinitionGenerator.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Mile.Project.Helpers;
+
+namespace Mile.UniCrt.Build.Tasks
+{
+    public class SimplifiedBaseDefinitionGenerator : Task
+    {
+        [Required]
+        public string BaseLibraryPath { get; set; }
+
+        [Required]
+        public string TargetPlatform { get; set; }
+
+        [Required]
+        public string OutputLibraryPath { get; set; }
+
+        public override bool Execute()
+        {
+            ImageArchive.Archive Archive = ImageArchive.Parse(
+               BaseLibraryPath + @"\ucrt.lib");
+
+            if (Archive.Symbols != null)
+            {
+                Utilities.GenerateDefinitionFile(
+                    string.Format(
+                        @"{0}\Mile.UniCrt.SimplifiedBase.def",
+                        OutputLibraryPath),
+                    "ucrtbase.dll",
+                    ImageArchive.ListSymbols(
+                        Archive.Symbols,
+                        TargetPlatform == "x86"));
+            }
+
+            if (Archive.EcSymbols != null)
+            {
+                Utilities.GenerateDefinitionFile(
+                   string.Format(
+                       @"{0}\Mile.UniCrt.SimplifiedBase.Arm64EC.def",
+                       OutputLibraryPath),
+                   "ucrtbase.dll",
+                   ImageArchive.ListSymbols(
+                       Archive.EcSymbols));
+            }
+
+            return true;
+        }
+    }
+}

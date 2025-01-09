c91dc5d20649af2efee1f4705eab077b662d120f
MouriNaruto <Kenji.Mouri@outlook.com>
2025-01-09 20:35:52 +0800

    Move Mile.UniCrt.Wrapper specific implement from Mile.UniCrt.OriginalVCRuntimeWrapper to Mile.UniCrt.Wrapper.

--- Mile.UniCrt.Wrapper/Mile.UniCrt.Wrapper.cpp
+++ Mile.UniCrt.Wrapper/Mile.UniCrt.Wrapper.cpp
@@ -10,7 +10,59 @@
 
 #include <Windows.h>
 
-EXTERN_C int WINAPI MileUniCrtWrapperDummyFunction()
+#ifdef _DEBUG
+
+#include <mbstring.h>
+
+extern "C" unsigned char* __cdecl _mbsdup_dbg(
+    _In_ unsigned char const* _String,
+    _In_ int _BlockUse,
+    _In_opt_ char const* _FileName,
+    _In_ int _LineNumber)
 {
-    return 0;
+    UNREFERENCED_PARAMETER(_BlockUse);
+    UNREFERENCED_PARAMETER(_FileName);
+    UNREFERENCED_PARAMETER(_LineNumber);
+
+    return ::_mbsdup(_String);
 }
+
+#endif // _DEBUG
+
+#if defined _M_IX86
+
+#include <eh.h>
+#include <winternl.h>
+
+typedef void(__fastcall* PCOOKIE_CHECK)(UINT_PTR);
+
+extern "C" EXCEPTION_DISPOSITION __cdecl _except_handler4(
+    _In_ PEXCEPTION_RECORD ExceptionRecord,
+    _In_ PEXCEPTION_REGISTRATION_RECORD EstablisherFrame,
+    _Inout_ PCONTEXT ContextRecord,
+    _Inout_ PVOID DispatcherContext);
+
+extern "C" EXCEPTION_DISPOSITION __cdecl _except_handler4_common(
+    _In_ PUINT_PTR CookiePointer,
+    _In_ PCOOKIE_CHECK CookieCheckFunction,
+    _In_ PEXCEPTION_RECORD ExceptionRecord,
+    _In_ PEXCEPTION_REGISTRATION_RECORD EstablisherFrame,
+    _Inout_ PCONTEXT ContextRecord,
+    _Inout_ PVOID DispatcherContext)
+{
+    UNREFERENCED_PARAMETER(CookiePointer);
+    UNREFERENCED_PARAMETER(CookieCheckFunction);
+    return ::_except_handler4(
+        ExceptionRecord,
+        EstablisherFrame,
+        ContextRecord,
+        DispatcherContext);
+}
+
+// Empty stub for bypassing compilation.
+extern "C" BOOL __cdecl __intrinsic_abnormal_termination()
+{
+    return FALSE;
+}
+
+#endif // defined _M_IX86
--- Mile.UniCrt/Mile.UniCrt.OriginalVCRuntimeWrapper.cpp
+++ Mile.UniCrt/Mile.UniCrt.OriginalVCRuntimeWrapper.cpp
@@ -8,64 +8,9 @@
  * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
  */
 
-#include <Windows.h>
-
-#include <cstdlib>
-#include <cstring>
-#include <cwchar>
-
-// Empty stub implementation because this is only the wrapper to the another
-// Universal C Runtime implementation.
-extern "C" char** __dcrt_initial_narrow_environment = nullptr;
-
-// Empty stub implementation because this is only the wrapper to the another
-// Universal C Runtime implementation.
-extern "C" wchar_t* __cdecl __dcrt_get_wide_environment_from_os()
-{
-    return nullptr;
-}
-
-#if defined _M_IX86
-
-#include <eh.h>
-#include <winternl.h>
-
-typedef void(__fastcall* PCOOKIE_CHECK)(UINT_PTR);
-
-extern "C" EXCEPTION_DISPOSITION __cdecl _except_handler4(
-    _In_ PEXCEPTION_RECORD ExceptionRecord,
-    _In_ PEXCEPTION_REGISTRATION_RECORD EstablisherFrame,
-    _Inout_ PCONTEXT ContextRecord,
-    _Inout_ PVOID DispatcherContext);
-
-extern "C" EXCEPTION_DISPOSITION __cdecl _except_handler4_common(
-    _In_ PUINT_PTR CookiePointer,
-    _In_ PCOOKIE_CHECK CookieCheckFunction,
-    _In_ PEXCEPTION_RECORD ExceptionRecord,
-    _In_ PEXCEPTION_REGISTRATION_RECORD EstablisherFrame,
-    _Inout_ PCONTEXT ContextRecord,
-    _Inout_ PVOID DispatcherContext)
-{
-    UNREFERENCED_PARAMETER(CookiePointer);
-    UNREFERENCED_PARAMETER(CookieCheckFunction);
-    return ::_except_handler4(
-        ExceptionRecord,
-        EstablisherFrame,
-        ContextRecord,
-        DispatcherContext);
-}
-
-// Empty stub for bypassing compilation.
-extern "C" BOOL __cdecl __intrinsic_abnormal_termination()
-{
-    return FALSE;
-}
-
-#endif // defined _M_IX86
-
 #if defined _M_AMD64
 
-#include <eh.h>
+#include <Windows.h>
 
 // Dummy stub for bypassing ARM64EC compilation.
 extern "C" void __MileUniCrtMakeCompilerGenerateThunks()

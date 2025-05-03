c0270d710377711ec4cbb3c5066affe7980f5784
MouriNaruto <Kenji.Mouri@outlook.com>
2025-05-03 15:01:06 +0800

    Continue to implement Mile.UniCrt.WrapperDefinitionGenerator.WeakSymbolsObjectGenerator.

--- Mile.UniCrt.WrapperDefinitionGenerator/WeakSymbolsObjectGenerator.cs
+++ Mile.UniCrt.WrapperDefinitionGenerator/WeakSymbolsObjectGenerator.cs
@@ -171,12 +171,180 @@ namespace Mile.UniCrt.WrapperDefinitionGenerator
             public readonly byte[] Unused;
         }
 
+        private const ushort IMAGE_FILE_MACHINE_UNKNOWN = 0x0000;
+
+        /// <summary>
+        /// At the beginning of an object file, or immediately after the
+        /// signature of an image file, is a standard COFF file header in the
+        /// following format. Note that the Windows loader limits the number of
+        /// sections to 96.
+        /// </summary>
+        /// <see cref="https://learn.microsoft.com/en-us/windows/win32/debug/pe-format#coff-file-header-object-and-image"/>
         [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
-        private struct WeakSymbolItem
+        private struct ImageFileHeader
         {
-            public ImageSymbol TargetSymbol;
-            public ImageSymbol AliasSymbol;
-            public ImageAuxWeakSymbol AuxSymbol;
+            /// <summary>
+            /// The number that identifies the type of target machine. For more
+            /// information, see Machine Types.
+            /// </summary>
+            public ushort Machine;
+
+            /// <summary>
+            /// The number of sections. This indicates the size of the section
+            /// table, which immediately follows the headers.
+            /// </summary>
+            public short NumberOfSections;
+
+            /// <summary>
+            /// The low 32 bits of the number of seconds since 00:00 January 1,
+            /// 1970 (a C run-time time_t value), which indicates when the file
+            /// was created.
+            /// </summary>
+            public int TimeDateStamp;
+
+            /// <summary>
+            /// The file offset of the COFF symbol table, or zero if no COFF
+            /// symbol table is present. This value should be zero for an image
+            /// because COFF debugging information is deprecated.
+            /// </summary>
+            public uint PointerToSymbolTable;
+
+            /// <summary>
+            /// The number of entries in the symbol table. This data can be used
+            /// to locate the string table, which immediately follows the symbol
+            /// table. This value should be zero for an image because COFF
+            /// debugging information is deprecated.
+            /// </summary>
+            public uint NumberOfSymbols;
+
+            /// <summary>
+            /// The size of the optional header, which is required for
+            /// executable files but not for object files. This value should be
+            /// zero for an object file. For a description of the header format,
+            /// see Optional Header (Image Only).
+            /// </summary>
+            public ushort SizeOfOptionalHeader;
+
+            /// <summary>
+            /// The flags that indicate the attributes of the file. For specific
+            /// flag values, see Characteristics.
+            /// </summary>
+            public ushort Characteristics;
+        }
+
+        private const uint IMAGE_SCN_LNK_INFO = 0x00000200;
+        private const uint IMAGE_SCN_LNK_REMOVE = 0x00000800;
+
+        /// <summary>
+        /// Each row of the section table is, in effect, a section header. This
+        /// table immediately follows the optional header, if any. This
+        /// positioning is required because the file header does not contain a
+        /// direct pointer to the section table. Instead, the location of the
+        /// section table is determined by calculating the location of the first
+        /// byte after the headers. Make sure to use the size of the optional
+        /// header as specified in the file header.
+        /// The number of entries in the section table is given by the
+        /// NumberOfSections field in the file header. Entries in the section
+        /// table are numbered starting from one (1). The code and data memory
+        /// section entries are in the order chosen by the linker.
+        /// In an image file, the VAs for sections must be assigned by the
+        /// linker so that they are in ascending order and adjacent, and they
+        /// must be a multiple of the SectionAlignment value in the optional
+        /// header.
+        /// Each section header (section table entry) has the following format,
+        /// for a total of 40 bytes per entry.
+        /// </summary>
+        /// <see cref="https://learn.microsoft.com/en-us/windows/win32/debug/pe-format#section-table-section-headers"/>
+       [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+        private struct ImageSectionHeader
+        {
+            /// <summary>
+            /// An 8-byte, null-padded UTF-8 encoded string. If the string is
+            /// exactly 8 characters long, there is no terminating null. For
+            /// longer names, this field contains a slash (/) that is followed
+            /// by an ASCII representation of a decimal number that is an offset
+            /// into the string table. Executable images do not use a string
+            /// table and do not support section names longer than 8 characters.
+            /// Long names in object files are truncated if they are emitted to
+            /// an executable file.
+            /// </summary>
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+            public byte[] Name;
+
+            /// <summary>
+            /// The total size of the section when loaded into memory. If this
+            /// value is greater than SizeOfRawData, the section is zero-padded.
+            /// This field is valid only for executable images and should be set
+            /// to zero for object files.
+            /// </summary>
+            public uint VirtualSize;
+
+            /// <summary>
+            /// For executable images, the address of the first byte of the
+            /// section relative to the image base when the section is loaded
+            /// into memory. For object files, this field is the address of the
+            /// first byte before relocation is applied; for simplicity,
+            /// compilers should set this to zero. Otherwise, it is an arbitrary
+            /// value that is subtracted from offsets during relocation.
+            /// </summary>
+            public uint VirtualAddress;
+
+            /// <summary>
+            /// The size of the section (for object files) or the size of the
+            /// initialized data on disk (for image files). For executable
+            /// images, this must be a multiple of FileAlignment from the
+            /// optional header. If this is less than VirtualSize, the remainder
+            /// of the section is zero-filled. Because the SizeOfRawData field
+            /// is rounded but the VirtualSize field is not, it is possible for
+            /// SizeOfRawData to be greater than VirtualSize as well. When a
+            /// section contains only uninitialized data, this field should be
+            /// zero.
+            /// </summary>
+            public uint SizeOfRawData;
+
+            /// <summary>
+            /// The file pointer to the first page of the section within the
+            /// COFF file. For executable images, this must be a multiple of
+            /// FileAlignment from the optional header. For object files, the
+            /// value should be aligned on a 4-byte boundary for best
+            /// performance. When a section contains only uninitialized data,
+            /// this field should be zero.
+            /// </summary>
+            public uint PointerToRawData;
+
+            /// <summary>
+            /// The file pointer to the beginning of relocation entries for the
+            /// section. This is set to zero for executable images or if there
+            /// are no relocations.
+            /// </summary>
+            public uint PointerToRelocations;
+
+            /// <summary>
+            /// The file pointer to the beginning of line-number entries for the
+            /// section. This is set to zero if there are no COFF line numbers.
+            /// This value should be zero for an image because COFF debugging
+            /// information is deprecated.
+            /// </summary>
+            public uint PointerToLinenumbers;
+
+            /// <summary>
+            /// The number of relocation entries for the section. This is set to
+            /// zero for executable images.
+            /// </summary>
+            public ushort NumberOfRelocations;
+
+            /// <summary>
+            /// The number of line-number entries for the section. This value
+            /// should be zero for an image because COFF debugging information
+            /// is deprecated.
+            /// </summary>
+            public ushort NumberOfLinenumbers;
+
+            /// <summary>
+            /// The flags that describe the characteristics of the section. For
+            /// more information, see Section Flags.
+            /// </summary>
+            public uint Characteristics;
         }
 
         private static byte[] StructureToBytes(object Structure)
@@ -190,27 +358,129 @@ namespace Mile.UniCrt.WrapperDefinitionGenerator
             return Result;
         }
 
+        private static void WriteToStream(
+            Stream Stream,
+            byte[] Bytes)
+        {
+            Stream.Write(Bytes, 0, Bytes.Length);
+        }
+
+        public static byte[] CreateWeakSymbolObject(
+            SortedDictionary<string, string> WeakSymbols)
+        {
+            MemoryStream ObjectStream = new MemoryStream();
+
+            ImageSectionHeader SectionHeader = new ImageSectionHeader();
+            byte[] RawName = Encoding.UTF8.GetBytes(".drectve");
+            SectionHeader.Name = new byte[8];
+            Array.Copy(RawName, SectionHeader.Name, RawName.Length);
+            SectionHeader.Characteristics =
+                IMAGE_SCN_LNK_INFO | IMAGE_SCN_LNK_REMOVE;
+            ImageFileHeader FileHeader = new ImageFileHeader();
+            FileHeader.Machine = IMAGE_FILE_MACHINE_UNKNOWN;
+            FileHeader.NumberOfSections = 1;
+            FileHeader.TimeDateStamp = Convert.ToInt32(
+                new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds());
+            FileHeader.PointerToSymbolTable = Convert.ToUInt32(
+                Marshal.SizeOf(FileHeader) + Marshal.SizeOf(SectionHeader));
+            FileHeader.NumberOfSymbols = Convert.ToUInt32(
+                2 + (WeakSymbols.Count * 3));
+            FileHeader.SizeOfOptionalHeader = 0;
+            FileHeader.Characteristics = 0x0000;
+            WriteToStream(ObjectStream, StructureToBytes(FileHeader));
+            WriteToStream(ObjectStream, StructureToBytes(SectionHeader));
+
+            ImageSymbol ComponentId = new ImageSymbol();
+            ComponentId.ShortName = "@comp.id";
+            ComponentId.Value = 0x00FD784B;
+            ComponentId.SectionNumber = IMAGE_SYM_ABSOLUTE;
+            ComponentId.Type = IMAGE_SYM_TYPE_NULL;
+            ComponentId.StorageClass = IMAGE_SYM_CLASS_STATIC;
+            ComponentId.NumberOfAuxSymbols = 0;
+            WriteToStream(ObjectStream, StructureToBytes(ComponentId));
+
+            ImageSymbol Feature00 = new ImageSymbol();
+            Feature00.ShortName = "@feat.00";
+            Feature00.Value = 0x00000011;
+            Feature00.SectionNumber = IMAGE_SYM_ABSOLUTE;
+            Feature00.Type = IMAGE_SYM_TYPE_NULL;
+            Feature00.StorageClass = IMAGE_SYM_CLASS_STATIC;
+            Feature00.NumberOfAuxSymbols = 0;
+            WriteToStream(ObjectStream, StructureToBytes(Feature00));
+
+            int WeakSymbolIndex = 0;
+            int StringPoolIndex = sizeof(uint);
+            foreach (KeyValuePair<string, string> WeakSymbol in WeakSymbols)
+            {
+                ImageSymbol TargetSymbol = new ImageSymbol();
+                if (WeakSymbol.Value.Length > 8)
+                {
+                    TargetSymbol.LongName = StringPoolIndex;
+                    StringPoolIndex += WeakSymbol.Value.Length + 1;
+                }
+                else
+                {
+                    TargetSymbol.ShortName = WeakSymbol.Value;
+                }  
+                TargetSymbol.Value = 0;
+                TargetSymbol.SectionNumber = IMAGE_SYM_UNDEFINED;
+                TargetSymbol.Type = IMAGE_SYM_TYPE_NULL;
+                TargetSymbol.StorageClass = IMAGE_SYM_CLASS_EXTERNAL;
+                TargetSymbol.NumberOfAuxSymbols = 0;
+                WriteToStream(ObjectStream, StructureToBytes(TargetSymbol));
+
+                ImageSymbol AliasSymbol = new ImageSymbol();
+                if (WeakSymbol.Key.Length > 8)
+                {
+                    AliasSymbol.LongName = StringPoolIndex;
+                    StringPoolIndex += WeakSymbol.Key.Length + 1;
+                }
+                else
+                {
+                    AliasSymbol.ShortName = WeakSymbol.Key;
+                }
+                AliasSymbol.Value = 0;
+                AliasSymbol.SectionNumber = IMAGE_SYM_UNDEFINED;
+                AliasSymbol.Type = IMAGE_SYM_TYPE_NULL;
+                AliasSymbol.StorageClass = IMAGE_SYM_CLASS_WEAK_EXTERNAL;
+                AliasSymbol.NumberOfAuxSymbols = 1;
+                WriteToStream(ObjectStream, StructureToBytes(AliasSymbol));
+
+                ImageAuxWeakSymbol AuxSymbol = new ImageAuxWeakSymbol();
+                AuxSymbol.TagIndex = 2 + (WeakSymbolIndex * 3);
+                AuxSymbol.Characteristics =
+                    ImageWeakExternSearchType.SearchAlias;
+                WriteToStream(ObjectStream, StructureToBytes(AuxSymbol));
+
+                ++WeakSymbolIndex;
+            }
+
+            WriteToStream(ObjectStream, BitConverter.GetBytes(StringPoolIndex));
+            foreach (KeyValuePair<string, string> WeakSymbol in WeakSymbols)
+            {
+                if (WeakSymbol.Value.Length > 8)
+                {
+                    byte[] Encoded = Encoding.UTF8.GetBytes(WeakSymbol.Value);
+                    WriteToStream(ObjectStream, Encoded);
+                    ObjectStream.WriteByte(0);
+                }
+                if (WeakSymbol.Key.Length > 8)
+                {
+                    byte[] Encoded = Encoding.UTF8.GetBytes(WeakSymbol.Key);
+                    WriteToStream(ObjectStream, Encoded);
+                    ObjectStream.WriteByte(0);
+                }
+            }
+
+            return ObjectStream.ToArray();
+        }
+
         public static void Test()
         {
-            WeakSymbolItem Item = new WeakSymbolItem();
-            Item.TargetSymbol = new ImageSymbol();
-            Item.TargetSymbol.ShortName = "#_access";
-            Item.TargetSymbol.Value = 0;
-            Item.TargetSymbol.SectionNumber = IMAGE_SYM_UNDEFINED;
-            Item.TargetSymbol.Type = IMAGE_SYM_TYPE_NULL;
-            Item.TargetSymbol.StorageClass = IMAGE_SYM_CLASS_EXTERNAL;
-            Item.TargetSymbol.NumberOfAuxSymbols = 0;
-            Item.AliasSymbol = new ImageSymbol();
-            Item.AliasSymbol.ShortName = "#access";
-            Item.AliasSymbol.Value = 0;
-            Item.AliasSymbol.SectionNumber = IMAGE_SYM_UNDEFINED;
-            Item.AliasSymbol.Type = IMAGE_SYM_TYPE_NULL;
-            Item.AliasSymbol.StorageClass = IMAGE_SYM_CLASS_WEAK_EXTERNAL;
-            Item.AliasSymbol.NumberOfAuxSymbols = 1;
-            Item.AuxSymbol = new ImageAuxWeakSymbol();
-            Item.AuxSymbol.TagIndex = 2 + ((1 - 1) * 3);
-            Item.AuxSymbol.Characteristics = ImageWeakExternSearchType.SearchAlias;
-            byte[] Bytes = StructureToBytes(Item);
+            SortedDictionary<string, string> WeakSymbols =
+                new SortedDictionary<string, string>();
+            WeakSymbols.Add("#access", "#_access");
+            byte[] Bytes = CreateWeakSymbolObject(WeakSymbols);
 
             File.WriteAllBytes("WeakSymbolItem.bin", Bytes);
 
